4fac829df65408da13a0617f1ef030836777b661
Yudha Muhammad Akbar <ya2208354@qu.edu.qa>
2026-07-21 11:33:24 +0300

    fix

--- notebooks/file_processing.py
+++ notebooks/file_processing.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
 dataset = pd.read_csv('data/raw/time_series_60min_singleindex.csv')
-germany_data = dataset['DE_load_actual_entsoe_transparency']
-germany_data = germany_data.iloc[1:]
+germany_data = dataset[['utc_timestamp','DE_load_actual_entsoe_transparency']]
+germany_data = germany_data.iloc[1:,:]
 
 germany_data.columns = [
     "timestamp",
